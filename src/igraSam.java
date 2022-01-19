1a64e1decfee9f85f261cf2964c198bd944b0e0b
AprioriKS <sounddummies@gmail.com>
2022-01-19 14:39:57 +0200

    up range to 100 1

--- src/igraSam.java
+++ src/igraSam.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 public class igraSam {
-        public static void main(String[] args) {
-            Scanner igra =new Scanner(System.in);
-            System.out.println("Вгадайка");
-            int rang=10;
-            int number = (int) (Math.random()*rang);
-            while(true){
+    public static void main(String[] args) {
+        Scanner igra = new Scanner(System.in);
+        System.out.println("Вгадайка");
+        int rang = 10;
+        int number = (int) (Math.random() * rang);
+        while (true) {
             System.out.println("Вгадай число від 0 до" + rang);
             int input_number = igra.nextInt();
 
         }
+    }
 }
+
